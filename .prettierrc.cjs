d614f8d7f264571bbc4e6770bcd045ffe2bc5a07
ff6347 <hash@ff6347.email>
2023-03-31 11:48:15 +0200

    chore: Configure pettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	jsxBracketSameLine: false,
+	printWidth: 80,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	trailingComma: 'es5',
+	parser: 'typescript',
+};
